547b93ec24809cdd6542b9506923ec82d8ddcdb8
jamessn688 <james@turbodogmedia.com>
2026-02-21 17:08:35 +0000

    Update vite.config.ts

--- 0221debtadv/vite.config.ts
+++ 0221debtadv/vite.config.ts
@@ -6,24 +6,24 @@ import { componentTagger } from "lovable-tagger";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: "/0221debtadv/",
-});
+}));
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
